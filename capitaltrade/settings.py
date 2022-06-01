0d10366b8a5800db7944b4eb64517c67f1613bc8
dkonti <richardmasondgreat@gmail.com>
2022-06-01 16:09:12 +0100

    picture change

--- capitaltrade/settings.py
+++ capitaltrade/settings.py
@@ -79,19 +79,12 @@ WSGI_APPLICATION = 'capitaltrade.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'maxtradedb',
-        'USER': 'maxtrade_admin',
-        'PASSWORD': 'mellykonti4',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
